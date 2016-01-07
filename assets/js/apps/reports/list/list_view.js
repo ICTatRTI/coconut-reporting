db8360806dc0857341a7b90f8211d7c2eebe523f
sloo <sloo@rti.org>
2016-01-07 14:41:18 -0500

    Add dropdown effect within reports view

--- assets/js/apps/reports/list/list_view.js
+++ assets/js/apps/reports/list/list_view.js
@@ -3,6 +3,20 @@ CocoManager.module("ReportsApp.List", function(List, CocoManager, Backbone, Mari
   List.ReportView = Marionette.ItemView.extend({
 	className: "report-content",
 	template: "#report-template",
+	
+	  events: {
+	  	"click div.dropDownBtn": "showAnalysisSection"
+	  },
+	  
+  	showAnalysisSection: function(e){
+  		e.preventDefault();
+		var $target = $(e.currentTarget); 
+    	$target.next("div.dropdown-section").slideToggle();
+		
+		var iconStatus = $target.find("i").text()== "file_download" ? "file_upload" : "file_download";
+		$target.find("i").text(iconStatus);
+  	}
+	  
   });
   
   List.ReportOptionsView = Marionette.ItemView.extend({
@@ -11,14 +25,12 @@ CocoManager.module("ReportsApp.List", function(List, CocoManager, Backbone, Mari
 	  
 	events: {
 	      "click span#filters-drop": "showDateFilter"
-
 	},
 	
 	showDateFilter: function(e){
 		e.preventDefault();
 		$("div#filters-section").slideToggle();
 	}
-	
 	
   });  
 
