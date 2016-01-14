f4d2b645214657fc2da9a492bbf72671991a5403
sloo <sloo@rti.org>
2016-01-14 13:39:44 -0500

    Added mockup pages for Activities

--- assets/js/apps/activity/show/show_controller.js
+++ assets/js/apps/activity/show/show_controller.js
@@ -1,12 +1,16 @@
 CocoManager.module("ActivityApp.Show", function(Show, CocoManager, Backbone, Marionette, $, _){
   Show.Controller = {
-	showActivity: function(activity_title){			  
+	showActivity: function(activity_title){
+		var report_name = activity_title == null ? "" : activity_title+".html";
+		// Loading mockup html pages
+		$("#activity-template").load(CocoManager.config.templates + report_name, function(){			  
 			Marionette.TemplateCache.clear("#activity-template","activity-options-template");
 	  	    var activityHeadView = new Show.ActivityOptionsView();			
 			var activityView = new Show.ActivityView()
 
 		    CocoManager.dashboard.dashHeaderRegion.show(activityHeadView);
-		    CocoManager.dashboard.dashMainRegion.show(activityView);	
-	    }  
+		    CocoManager.dashboard.dashMainRegion.show(activityView);
+		});		
+	  }  
 	}	
 });
