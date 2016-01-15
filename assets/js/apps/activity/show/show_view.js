de2ab49a01c06d7ec0db807e01d0faef85c78b5a
sloo <sloo@rti.org>
2016-01-15 10:03:15 -0500

    Adding select-all feature for activities:sms

--- assets/js/apps/activity/show/show_view.js
+++ assets/js/apps/activity/show/show_view.js
@@ -3,11 +3,21 @@ CocoManager.module("ActivityApp.Show", function(Show, CocoManager, Backbone, Mar
   Show.ActivityView = Marionette.ItemView.extend({
 	className: "activity-content",
 	template: "#activity-template",
+	  events: {
+	  	"click input#check-all": "selectAllCheckbox"
+	  },
+	  selectAllCheckbox: function(e){
+ 		 if ($(e.currentTarget).prop('checked')){
+ 		 	$("input.phone-number").prop('checked', true);
+ 		 } else {
+ 		    $("input.phone-number").prop('checked', false);
+ 	     }
+	  }
   });
 
   Show.ActivityOptionsView = Marionette.ItemView.extend({
 	  className: "report-header",  
-	template: "#activity-options-template",
+	template: "#activity-options-template"
   });
 
 });
